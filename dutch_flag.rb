49dbc3e3a4537cd4a45b3427e0789bbc73ebc21b
mattwesthoff <matt@enlearn.org>
2016-07-31 21:06:05 -0700

    start a real implementation

--- dutch_flag.rb
+++ dutch_flag.rb
@@ -1,5 +1,19 @@
 class DutchFlag
   def sort(array)
-    array.sort
+    zeroes = 0
+    # ones = 0
+    # twos = array.length
+
+    array.length.times do
+      swap!(array, 0, 1) if array[zeroes] == 1
+    end
+
+    array
+  end
+
+  def swap!(array, a, b)
+    t = array[a]
+    array[a] = array[b]
+    array[b] = t
   end
 end
