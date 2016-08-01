7b04a8ce2a00920c91986ecc5930f9fa75e71988
mattwesthoff <matt@enlearn.org>
2016-07-31 21:14:04 -0700

    a little cleanup

--- dutch_flag.rb
+++ dutch_flag.rb
@@ -1,19 +1,31 @@
 class DutchFlag
-  def sort(array)
-    zeroes = 0
-    # ones = 0
-    # twos = array.length
-
-    array.length.times do
-      swap!(array, 0, 1) if array[zeroes] == 1
-    end
-
-    array
+  def initialize(array)
+    @array = array
   end
 
-  def swap!(array, a, b)
-    t = array[a]
-    array[a] = array[b]
-    array[b] = t
+  def sort
+    @zeroes = 0
+    # ones = 0
+    @twos = @array.length - 1
+
+    @array.length.times do
+      swap!(0, 1) if @array[@zeroes] == 1
+      zero_to_twos if @array[@zeroes] == 2
+    end
+
+    @array
+  end
+
+  private
+
+  def zero_to_twos
+    swap!(@zeroes, @twos)
+    @twos -= 1
+  end
+
+  def swap!(a, b)
+    t = @array[a]
+    @array[a] = @array[b]
+    @array[b] = t
   end
 end
--- dutch_flag_test.rb
+++ dutch_flag_test.rb
@@ -2,17 +2,18 @@ require_relative 'dutch_flag'
 require 'test/unit'
 
 class DutchFlagTest < Test::Unit::TestCase
-  def setup
-    @test_obj = DutchFlag.new
-  end
-
   def test_already_sorted
-    result = @test_obj.sort([0, 1])
+    result = DutchFlag.new([0, 1]).sort
     assert_equal [0, 1], result
   end
 
   def test_sorts_one_zero
-    result = @test_obj.sort([1, 0])
+    result = DutchFlag.new([1, 0]).sort
     assert_equal [0, 1], result
+  end
+
+  def test_sorts_two_zero_one
+    result = DutchFlag.new([2, 0, 1]).sort
+    assert_equal [0, 1, 2], result
   end
 end
