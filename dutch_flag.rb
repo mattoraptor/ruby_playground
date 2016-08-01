128d78b1c5371e38d20809e5e88c06ffced658d7
mattwesthoff <matt@enlearn.org>
2016-07-31 21:58:07 -0700

    don't need those to be class vars

--- dutch_flag.rb
+++ dutch_flag.rb
@@ -4,35 +4,32 @@ class DutchFlag
   end
 
   def sort
-    @front = 0
-    @middle = 0
-    @back = @array.length - 1
+    front = 0
+    middle = 0
+    back = @array.length - 1
 
     @array.length.times do
-      if @middle > @back
-        break
-
-      elsif @array[@front] == 0
-        @front += 1
-        @middle += 1
-      elsif @array[@front] == 2
-        swap!(@front, @back)
-        @back -= 1
-      elsif @array[@back] == 0
-        swap!(@front, @back)
-        @front += 1
-        @middle += 1
-      elsif @array[@back] == 2
-        @back -= 1
-      elsif @array[@middle] == 0
-        swap!(@middle, @front)
-        @front += 1
-        @middle += 1
-      elsif @array[@middle] == 2
-        swap!(@middle, @back)
-        @back -= 1
-      elsif @array[@middle] == 1
-        @middle += 1
+      if @array[front] == 0
+        front += 1
+        middle += 1
+      elsif @array[front] == 2
+        swap!(front, back)
+        back -= 1
+      elsif @array[back] == 0
+        swap!(front, back)
+        front += 1
+        middle += 1
+      elsif @array[back] == 2
+        back -= 1
+      elsif @array[middle] == 0
+        swap!(middle, front)
+        front += 1
+        middle += 1
+      elsif @array[middle] == 2
+        swap!(middle, back)
+        back -= 1
+      elsif @array[middle] == 1
+        middle += 1
       end
     end
 
